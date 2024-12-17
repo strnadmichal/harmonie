c3864a572d42b41f8fbe2477d173f3df47201a03
Michal Strnad <michalstrnad@MacBook-Air.local>
2024-12-17 16:18:36 +0100

    Adds navigation bar

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'nav': ['Alice'],
+    }
+  },
+  plugins: [],
+}
+
